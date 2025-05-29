fa0e92a91667d170de8d41e647c4bd731a2128c7
takunojima <147133506+takunojima@users.noreply.github.com>
2025-05-29 18:34:43 +0900

    Move files to root directory

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
 echo "Current directory: $(pwd)"
 echo "Listing files: $(ls -la)"
+
+# Install Python dependencies
 pip install -r requirements.txt
-mkdir -p instance
+
+# Create necessary directories if they don't exist
+mkdir -p instance 
